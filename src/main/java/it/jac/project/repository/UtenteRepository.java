256c8d51f7e11bcf803f8b7602450c52d45e8c4a
Louis-Guiniot <louis99679@gmail.com>
2021-03-17 15:38:05 +0100

    fix repository utente, mancava annotazione @repository

--- src/main/java/it/jac/project/repository/UtenteRepository.java
+++ src/main/java/it/jac/project/repository/UtenteRepository.java
@@ -1,8 +1,11 @@
 package it.jac.project.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import it.jac.project.entity.Utente;
 
+@Repository
 public interface UtenteRepository extends CrudRepository<Utente, Integer>{
 	Utente findByUsernameAndPassword(String username, String password);
 }
