a08dcabd320d5cca622f0966a045da7fd1b95287
Louis-Guiniot <louis99679@gmail.com>
2021-03-17 15:26:26 +0100

    creata entità utente

---
+++ src/main/java/it/jac/project/entity/Utente.java
@@ -0,0 +1,37 @@
+package it.jac.project.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity 
+@Table(name = "utente") 	
+@Data
+public class Utente {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "cognome")
+	private String cognome;
+	
+	@Column(name = "username")
+	private String username;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "genere")
+	private String genere;
+
+}
