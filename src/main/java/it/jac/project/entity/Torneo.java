08daaedde920a06339b8d7ce6d4ce36a9a3bc011
Louis-Guiniot <louis99679@gmail.com>
2021-03-17 16:47:18 +0100

    creata entità torneo

---
+++ src/main/java/it/jac/project/entity/Torneo.java
@@ -0,0 +1,55 @@
+package it.jac.project.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity 
+@Table(name = "torneo") 	
+@Data
+public class Torneo {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "nome_torneo")
+	private String nome;
+	
+	@Column(name = "gioco")
+	private String gioco;
+	
+	@Column(name = "piattaforma")
+	private String piattaforma;
+	
+	@Column(name = "capienza")
+	private int capienza;
+	
+	@Column(name = "iscrizioni")
+	private int iscrizioni;
+	
+	@Column(name = "posti_liberi")
+	private int postiLiberi;
+	
+	@Column(name = "partite_da_giocare")
+	private int partite;
+	
+	@Column(name = "quota_iscrizione")
+	private int quota;
+	
+	@Column(name = "premio_primo")
+	private String premioPrimo;
+	
+	@Column(name = "premio_secondo")
+	private String premioSecondo;
+	
+	@Column(name = "premio_terzo")
+	private String premioTerzo;
+	
+}
