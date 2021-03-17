c66fc4e7627d5d43689f211989261c740bdd7e42
Louis-Guiniot <louis99679@gmail.com>
2021-03-17 17:48:29 +0100

    aggiunta annotazione @repository

--- src/main/java/it/jac/project/repository/TorneoRepository.java
+++ src/main/java/it/jac/project/repository/TorneoRepository.java
@@ -3,9 +3,11 @@ package it.jac.project.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import it.jac.project.entity.Torneo;
 
+@Repository
 public interface TorneoRepository extends CrudRepository<Torneo, Integer>{
 	List<Torneo> findAllByGioco(String gioco);
 	List<Torneo> findAllByPiattaforma(String piattaforma);
