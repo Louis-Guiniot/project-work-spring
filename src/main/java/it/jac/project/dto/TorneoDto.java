872a42ecccff5c7f9ebdf343fe9246ee86e602fc
Louis-Guiniot <louis99679@gmail.com>
2021-03-17 17:43:37 +0100

    fix torneo dto con build

--- src/main/java/it/jac/project/dto/TorneoDto.java
+++ src/main/java/it/jac/project/dto/TorneoDto.java
@@ -1,5 +1,8 @@
 package it.jac.project.dto;
 
+import org.springframework.beans.BeanUtils;
+
+import it.jac.project.entity.Torneo;
 import lombok.Data;
 
 @Data
@@ -18,5 +21,13 @@ public class TorneoDto {
 	private String premioPrimo;
 	private String premioSecondo;
 	private String premioTerzo;
+	
+	public static TorneoDto build(Torneo torneo) {
+
+		TorneoDto result = new TorneoDto();
+		BeanUtils.copyProperties(torneo, result);
+
+		return result;
+	}
 
 }
