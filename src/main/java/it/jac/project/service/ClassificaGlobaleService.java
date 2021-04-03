b2b780fa07a742cdb0a3e278bc271d30ab3bb5a5
Louis-Guiniot <61980982+Louis-Guiniot@users.noreply.github.com>
2021-04-03 02:29:17 +0200

    metodo finGlobale che richiama findAll repo

--- src/main/java/it/jac/project/service/ClassificaGlobaleService.java
+++ src/main/java/it/jac/project/service/ClassificaGlobaleService.java
@@ -1,10 +1,19 @@
 package it.jac.project.service;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.jac.project.dto.ClassificaDto;
+import it.jac.project.dto.ClassificaGlobaleDto;
+import it.jac.project.dto.ResponseDto;
+import it.jac.project.entity.Classifica;
+import it.jac.project.entity.ClassificaGlobale;
 import it.jac.project.repository.ClassificaGlobaleRepository;
 
 @Service
@@ -14,6 +23,37 @@ public class ClassificaGlobaleService {
 
 	@Autowired
 	ClassificaGlobaleRepository classificaGlobaleRepository;
+	
+	public ResponseDto<List<ClassificaGlobaleDto>> findGlobale() {
+
+		ResponseDto<List<ClassificaGlobaleDto>> response = new ResponseDto<List<ClassificaGlobaleDto>>();
+
+		List<ClassificaGlobaleDto> result = new ArrayList<>();
+
+		try {
+
+			Iterator<ClassificaGlobale> iterator = this.classificaGlobaleRepository.findAll().iterator();
+
+			while (iterator.hasNext()) {
+
+				ClassificaGlobale classificaGlobale = iterator.next();
+				
+					result.add(ClassificaGlobaleDto.build(classificaGlobale));
+
+			}
+
+			response.setResult(result);
+			response.setResultTest(true);
+
+		} catch (Exception e) {
+
+			response.setError("Nessuna classifica globale trovata.");
+
+		}
+		
+		return response;
+
+	}
 	
 	
 }
